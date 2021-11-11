a19c4c8a48612d554aa3230b28f20966fe492891
willian-vieira <williannvieira.w@gmail.com>
2021-11-11 19:33:32 -0300

    Retornando GET list om array e v-for

--- src/App.vue
+++ src/App.vue
@@ -37,11 +37,11 @@
 
         <tbody>
 
-          <tr>
+          <tr v-for="produto of produtos" :key="produto.id">
 
-            <td>Arduino</td>
-            <td>100</td>
-            <td>50.00</td>
+            <td>{{ produto.nome }}</td>
+            <td>{{ produto.quantidade }}</td>
+            <td>{{ produto.valor }}</td>
             <td>
               <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
               <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
@@ -63,9 +63,16 @@ import Produto from './services/produtos';
 
 export default {
 
+  data() {
+    return {
+      produtos: []
+    }
+  },
+
   mounted() {
     Produto.listar().then(response => {
-      console.log(response);
+      console.log(response.data)
+      this.produtos = response.data
     })
   }
 
